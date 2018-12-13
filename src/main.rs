3d70d2c137cd821ce563fd27235c4a210dfd2072
Robert Roskam <raiderrobert@gmail.com>
2018-12-13 14:38:41 -0500

    added in command parsing functions

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::net::{TcpListener, TcpStream};
 use std::io::{BufReader, BufRead, Write};
 use std::thread;
-
+use std::string::{String};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
@@ -26,6 +26,9 @@ fn handle_client(mut stream: TcpStream) {
         while reader.read_line(&mut buf).unwrap_or(0) > 0 {
             {
                 let line = buf.trim_right();
+
+                let commands: Vec<&str> = line.split(' ').collect();
+                parse_command(commands[0].to_string());
                 println!("{}", line);
             }
             buf.clear();
@@ -35,4 +38,15 @@ fn handle_client(mut stream: TcpStream) {
     let response = "+OK\r\n";
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
+}
+
+fn parse_command(command: String) -> fn(){
+    match String::from_str(command) {
+        "GET" => get
+
+    }
+}
+
+fn get(){
+
 }
